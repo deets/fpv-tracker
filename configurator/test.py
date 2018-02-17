7e29590336ffec55f6bcd55f06007102d2ebc3d6
Diez B. Roggisch <deets@web.de>
2018-02-17 13:59:24 +0100

    Decode current status protocol

--- configurator/test.py
+++ configurator/test.py
@@ -1,25 +1,66 @@
 import sys
+import struct
+
 from PyQt5 import QtGui, QtSvg, QtWidgets, QtSerialPort, QtCore
 
 class SerialPortReader(QtCore.QObject):
 
     TIMEOUT = 5000
 
-    def __init__(self, port, parent=None):
+    def __init__(self, protocol, port, parent=None):
         super().__init__(parent)
         self._port = port
         self._port.readyRead.connect(self._handle_ready_read)
         self._port.error.connect(self._handle_error)
+        self._protocol = protocol
 
 
     def _handle_ready_read(self):
-        print(self._port.readAll())
-        if not self._timer.isActive():
-            self._timer.start(self.TIMEOUT)
+        self._protocol.feed(self._port.readAll())
 
 
     def _handle_error(self, error):
         print("error occured", error)
+
+
+class SerialProtocol:
+    PAYLOAD = "IfHH"
+    LAYOUT =  PAYLOAD + "B"
+
+    def __init__(self):
+        self._buffer = b""
+
+
+    def feed(self, data):
+        self._buffer += bytes(data)
+        while self._buffer:
+            pos = self._buffer.index(b"{")
+            end = pos + struct.calcsize(self.LAYOUT) + 1
+            try:
+                right_boundary = chr(self._buffer[end])
+            except IndexError:
+                # we don't have yet enough data, just
+                # break
+                break
+            else:
+                if right_boundary == '}':
+                    self._parse_message(self._buffer[pos+1:end])
+                    self._buffer = self._buffer[end+1:]
+                else:
+                    # we didn't arrive at a correct boundary,
+                    # lets throw away the data in question
+                    self._buffer = self._buffer[pos+1:]
+
+
+    def _parse_message(self, message):
+        crc = message[-1]
+        h = 0
+        for c in message[:-1]:
+            h += c
+        h = h & 0xff
+        if h == crc:
+            # successfully decoded a message
+            print(struct.unpack(self.PAYLOAD, message[:-1]))
 
 
 def main():
@@ -33,7 +74,8 @@ def main():
     port = QtSerialPort.QSerialPort(port_info)
     port.open(QtCore.QIODevice.ReadWrite)
 
-    reader = SerialPortReader(port)
+    protocol = SerialProtocol()
+    reader = SerialPortReader(protocol, port)
 
     svgWidget = QtSvg.QSvgWidget('NASA_Logo.svg')
     svgWidget.setGeometry(50,50,759,668)
